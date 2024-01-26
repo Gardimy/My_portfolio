a601714d8d521d6fa88f1be847d2626947deaf1a
Gardimy Charles <gardimycharles889@gmail.com>
2024-01-25 21:01:10 -0500

    fix eslint issues

--- js/script.js
+++ js/script.js
@@ -1,66 +1,68 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.navbar a');
-    const menuIcon = document.querySelector('#menu-icon');
-    const navbar = document.querySelector('.navbar');
+/* global ScrollReveal, Typed */
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('.navbar a');
+  const menuIcon = document.querySelector('#menu-icon');
+  const navbar = document.querySelector('.navbar');
 
-            // Remove active class from all links
-            navLinks.forEach(link => link.classList.remove('active'));
+  navLinks.forEach((link) => {
+    link.addEventListener('click', function handleClick(event) {
+      event.preventDefault();
 
-            // Add active class to the clicked link
-            this.classList.add('active');
+      // Remove active class from all links
+      navLinks.forEach((link) => link.classList.remove('active'));
 
-            // Scroll to the target section
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
+      // Add active class to the clicked link
+      this.classList.add('active');
 
-            if (targetSection) {
-                targetSection.scrollIntoView({ behavior: 'smooth' });
-            }
+      // Scroll to the target section
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
 
-            // Close the menu and remove the toggle icon
-            menuIcon.classList.remove('bx-x');
-            navbar.classList.remove('active');
-        });
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+
+      // Close the menu and remove the toggle icon
+      menuIcon.classList.remove('bx-x');
+      navbar.classList.remove('active');
     });
+  });
 
-    let header = document.querySelector('header');
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  };
+
+  // Add a scroll event listener to close the menu on scroll
+  window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('active');
-    };
+    // Close the menu and remove the toggle icon on scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  });
 
-    // Add a scroll event listener to close the menu on scroll
-    window.addEventListener('scroll', function () {
-        header.classList.toggle('sticky', window.scrollY > 100);
+  ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+  });
 
-        // Close the menu and remove the toggle icon on scroll
-        menuIcon.classList.remove('bx-x');
-        navbar.classList.remove('active');
-    });
-});
+  ScrollReveal().reveal('.home-content, .heading, .heading2', { origin: 'top' });
+  ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-ScrollReveal({ 
-	//reset: true,
-	distance: '80px',
-	duration: 2000,
-	delay: 200,
-});
-
-ScrollReveal().reveal('.home-content, .heading, .heading2', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-
-const typed = new Typed('.multiple-text', {
-	strings: ['Frontend Developer', 'Backend Developer', 'Electronic Engineer'],
-	typeSpeed: 100,
-	backSpeed: 100,
-	backDelay: 1000,
-	loop: true
+  // eslint-disable-next-line no-unused-vars
+  const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Electronic Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
 });
